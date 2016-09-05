810c6c9fff2d2f636a1d184e44318280d2440446
Summ1995 <1943979275@qq.com>
2016-09-06 00:22:22 +0800

    数据库添加（未完成）

--- app_tool/src/main/java/com/example/MyClass.java
+++ app_tool/src/main/java/com/example/MyClass.java
@@ -11,16 +11,36 @@ public class MyClass {
 
     private static void creatTable() {
         Schema schema = new Schema("fulishe",1,"com.androidxx.yangjw.day38_greendao_curd_demo");
-        //添加表
+        //添加购物车表
         Entity shoppingcar = schema.addEntity("ShoppingCar");
         shoppingcar.addIdProperty().autoincrement();
+        shoppingcar.addStringProperty("goods_id");
         shoppingcar.addStringProperty("goods_english_name");
         shoppingcar.addStringProperty("goods_english_name");
         shoppingcar.addStringProperty("goods_name");
         shoppingcar.addStringProperty("shop_price");
         shoppingcar.addIntProperty("num");
+
+        // 添加收货地址表
         Entity addressInfo=schema.addEntity("AddressInfo");
+        addressInfo.addIdProperty().autoincrement();
+        addressInfo.addStringProperty("userNamne").notNull();
+        addressInfo.addStringProperty("cityInfo").notNull();
+        addressInfo.addStringProperty("address").notNull();
+        addressInfo.addStringProperty("phoneNum").notNull();
+        addressInfo.addStringProperty("locPhoneNum");
+        addressInfo.addStringProperty("postalcode").notNull();
+        addressInfo.addBooleanProperty("default");
+
+        //添加用户信息表
         Entity userInfo=schema.addEntity("UserInfo");
+        addressInfo.addIdProperty().autoincrement();
+        userInfo.addStringProperty("userName");
+        userInfo.addStringProperty("sex");
+        userInfo.addStringProperty("birthday");
+        userInfo.addStringProperty("phoneNum");
+        userInfo.addStringProperty("email");
+
         //自动生成
         try {
             new DaoGenerator().generateAll(schema,"../ProjectTest/app/src/main/java");
